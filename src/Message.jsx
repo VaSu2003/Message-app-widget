f1f280e1ae6426c598aedf7f93cd3c2957c392a4
Vaibhav <vaibhav.shukla98871174@gmail.com>
2023-08-03 15:32:43 +0530

    add a submitReply button and make it functional

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Message({message}) {
+
+  const [child,setChild] = useState(message)
   console.log(message)
 
   function addReply(id){
     document.getElementById('text').focus()
     let submitReply = document.createElement('button')
     submitReply.textContent = "Submit Reply"
+    submitReply.id = id
+    submitReply.onclick = (replyHandler)
     document.getElementById(id).appendChild(submitReply)
+  }
+
+  function replyHandler(e){
+    let newMessage = message.children.push({id:message.id + 0.1 , text:document.getElementById('text').value, children:[]})
+    document.getElementById('text').value = ''
+    console.log(message)
+    setChild(newMessage)
   }
 
   return (
